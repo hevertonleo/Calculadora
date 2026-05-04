335c6943c9497b1bf8745102b39924624dab73c6
Heverton <hevertonleo87@gmail.com>
2026-05-04 16:16:18 -0300

    feat:função de cálculo e erro

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ function adicionar(valor) {
 
 function limpar() {
     display.value = '';
+}function apagar() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calcular() {
+    try {
+        // eval() resolve a string matemática (ex: "2+2" vira 4)
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+        setTimeout(limpar, 1500);
+    }
 }
